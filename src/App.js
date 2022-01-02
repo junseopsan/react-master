fd9c2f86cbd9ec4b950364f38c299468f3da6b6d
san <metternichch@gmail.com>
2022-01-02 22:18:49 +0900

    as .. attrs({} 사용법

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import styled from 'styled-components'
 
+const Father = styled.div`
+  display: flex;
+`;
+const Input = styled.input.attrs({required: true})`
+  background-color: tomato;
+  width: 100px;
+  height:100px;
+`;
 
 function App() {
-  return 'asdaa'
+  return (
+    <Father as="header">
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      
+    </Father>
+  )
 }
 
 export default App;
