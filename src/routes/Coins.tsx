6a534771d00f38f80ca0f8d501ea6177712e1f89
junseopsan <junseopsan@gmail.com>
2023-02-26 15:18:03 +0900

    스테이트

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -14,16 +14,19 @@ const Header = styled.header`
   justify-content: center;
   align-items: center;
 `;
+
 const CoinsList = styled.ul``;
+
 const Coin = styled.li`
   background-color: white;
   color: ${props => props.theme.bgColor};
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -31,13 +34,21 @@ const Coin = styled.li`
     }
   }
 `;
+
 const Title = styled.h1`
   font-size: 48px;
   color: ${props => props.theme.accentColor};
 `;
+
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -72,7 +83,16 @@ function Coins() {
         <CoinsList>
           {coins.map(coin => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}>
+                <Img
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
